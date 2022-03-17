e37634eacacbbaf8deccbc9b54a7fd0ca3bcfdd5
Greg Spencer <gspencer@google.com>
2022-03-16 19:46:20 -0700

    Implements MenuBar widget.

--- packages/flutter/lib/src/widgets/shortcuts.dart
+++ packages/flutter/lib/src/widgets/shortcuts.dart
@@ -297,6 +297,11 @@ class LogicalKeySet extends KeySet<LogicalKeyboardKey> with Diagnosticable
     return keysEqual;
   }
 
+  @override
+  bool isActivatedBy(RawKeyEvent event, [RawKeyboard? state]) {
+    return triggers.contains(event.logicalKey) && accepts(event, state ?? RawKeyboard.instance);
+  }
+
   static final Set<LogicalKeyboardKey> _modifiers = <LogicalKeyboardKey>{
     LogicalKeyboardKey.alt,
     LogicalKeyboardKey.control,
@@ -502,6 +507,11 @@ class SingleActivator with Diagnosticable, MenuSerializableShortcut implements S
   }
 
   @override
+  bool isActivatedBy(RawKeyEvent event, [RawKeyboard? state]) {
+    return event.logicalKey == trigger && accepts(event, state ?? RawKeyboard.instance);
+  }
+
+  @override
   bool accepts(RawKeyEvent event, RawKeyboard state) {
     final Set<LogicalKeyboardKey> pressed = state.keysPressed;
     return event is RawKeyDownEvent
@@ -602,6 +612,11 @@ class CharacterActivator with Diagnosticable, MenuSerializableShortcut implement
   bool accepts(RawKeyEvent event, RawKeyboard state) {
     return event is RawKeyDownEvent
         && event.character == character;
+  }
+
+  @override
+  bool isActivatedBy(RawKeyEvent event, [RawKeyboard? state]) {
+    return accepts(event, state ?? RawKeyboard.instance);
   }
 
   @override
